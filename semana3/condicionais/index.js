91303577a26d4145c277d0bec9709e16025c8971
marcosmaiaideia <marcosmaia@gmail.com>
2021-02-24 18:43:55 -0300

    Aula 12 condicionais - exercícios de interpretação OK

---
+++ semana3/condicionais/index.js
@@ -0,0 +1,33 @@
+
+
+//EXERCÍCIO 1
+/*
+O código deste exercício recebe um dado do usuário pelo prompt e armazena na const respostaDoUsuario
+Na sequencia pega esse valor e transforma em number, que armazena a referência na const numero
+
+O teste basicamente busca saver se o resto da divisão do número - devidamente convertida pra number - 
+inserido pelo usuário é PAR, pois se for PAR, terá resto zero, sendo igual em tipo e valor
+ao número 0 que está no if. E se tiver resto 1, será IMPAR, e imprimirá no CONSOLE que não passou no teste. 
+
+*/
+
+
+//EXERCÍCIO 2
+/*
+a. O código acima, retorna ao usuário o valor da fruta que ele digita no prompt.
+b. 2.25
+c. 5  // pois vai pegar o valor seguinte - e último - que é o default
+*/
+
+
+//EXERCÍCIO 3
+/*
+a. recebendo um valor digitado pelo usuário e convertendo-o para o tipo number.
+b. "Esse número passou no teste". Se fosse -10, apenas um erro.
+c. Sim. Justamente porque a let mensagem foi criada dentro do escopo do IF, não estando 
+disponível no escopo global onde ela foi passada como argumento do console.log.
+O console.log com a mensagem precisaria estar dentro do if, pare receber o valor da variável
+que foi declarada neste mesmo escopo.
+
+
+*/
