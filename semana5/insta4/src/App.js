b32e5c8d8325d5baaf26c34d8cf5ff6541863213
marcosmaiaideia <marcosmaia@gmail.com>
2021-03-13 17:35:59 -0300

    implementação do form para criar post

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -1,29 +1,96 @@
 import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
+import styled from 'styled-components'
+
+// const FormStyle = styled`
+//   input, button {
+//     margin: 10px 0;
+//   }
+// `
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-                <Post
-          nomeUsuario={'Marcos'}
-          fotoUsuario={'https://picsum.photos/49/49'}
-          fotoPost={'https://picsum.photos/199/249'}
-        />
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/48/48'}
-          fotoPost={'https://picsum.photos/198/248'}
-        />
 
+  state = {
+    posts:[
+      {
+        nomeUsuario:'Paulinha',
+        fotoUsuario:'https://picsum.photos/50/50',
+        fotoPost:'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario:'Marcos',
+        fotoUsuario:'https://picsum.photos/49/49',
+        fotoPost:'https://picsum.photos/199/249'
+      },
+      {
+        nomeUsuario:'Jose',
+        fotoUsuario:'https://picsum.photos/48/48',
+        fotoPost:'https://picsum.photos/198/248'
+      }
+    ],
+    valorInputNomeUsuario: '',
+    valorInputFotoUsuario: '',
+    valorInputFotoPost:''
+  }
+
+  adicionarPost = () =>{
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNomeUsuario,
+      fotoUsuario: this.state.valorInputfotoUsuario,
+      fotoPost: this.state.valorInputFotoPost
+    }
+    const novoPosts = [...this.state.posts, novoPost]
+
+    this.setState({ posts: novoPosts })
+  }
+
+
+  onChangeInputNomeUsuario = (event) =>{
+    this.setState({ valorInputNomeUsuario: event.target.value})
+  }
+  onChangeInputfotoUsuario = (event) =>{
+    this.setState({ valorInputfotoUsuario: event.target.value})
+  }
+  onChangeInputFotoPost = (event) =>{
+    this.setState({ valorInputFotoPost: event.target.value})
+  }
+
+  render() {
+      return(
+        <div className={'app-container'}> 
+        {/* <Estilos> */}
+          {/* <FormStyle> */}
+            <input
+              value={this.state.valorInputNomeUsuario}
+              onChange={this.onChangeInputNomeUsuario}
+              placeholder={'Nome'}
+            /> 
+            {/* </FormStyle> */}
+            <input 
+              value={this.state.valorInputfotoUsuario}
+              onChange={this.onChangeInputfotoUsuario}
+              placeholder={'Imagem do Usuário'}
+            />
+            <input 
+              value={this.state.valorInputFotoPost}
+              onChange={this.onChangeInputFotoPost}
+              placeholder={'Foto do Post'}
+            />
+            {/* </FormStyle> */}
+            <button onClick={this.adicionarPost}>Postar</button>
+
+          {this.state.posts.map((post) => {
+              return <Post 
+                key={post.nomeUsuario}
+                nomeUsuario={post.nomeUsuario}
+                fotoUsuario={post.fotoUsuario}
+                fotoPost={post.fotoPost.toString()}
+              />
+          })}
+          {/* </Estilos> */}
       </div>
-    );
+      ) 
   }
 }
 
--- semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -3,11 +3,13 @@ import './SecaoComentario.css'
 
 export class SecaoComentario extends Component {
 	state = {
-
+		valorComentario: ''
 	}
 
-	onChangeComentario() {
-
+	onChangeComentario = (event) => {
+		this.setState({
+			valorComentario: event.target.value
+		})
 	}
 
 	render() {
@@ -15,7 +17,7 @@ export class SecaoComentario extends Component {
 			<input
 				className={'input-comentario'}
 				placeholder={'Comentário'}
-				value={''}
+				value={this.state.valorComentario}
 				onChange={this.onChangeComentario}
 			/>
 			<button onClick={this.props.aoEnviar}>Enviar</button>
